e3f8d85ae5a069436008259035933a047ce69b46
Patrick Punch <punchp@oregonstate.edu>
2024-10-29 14:51:49 -0700

    completed Mult.asm for project 04

--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -7,4 +7,19 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+@2
+M=0 // Initialize M[2] to 0 
+(LOOP)
+@1
+D=M
+@IS_ZERO
+D;JEQ // Jump to the end if M[1] has reached zero
+@0
+D=M
+@2
+M=D+M // Add M[0] to the total product
+@1
+M=M-1 // Decrement M[1]
+@LOOP // Jump up to add again
+(IS_ZERO)
+0;JMP
